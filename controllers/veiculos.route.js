7516fc1bd4adeb8874f61ea7994a92365a5c4736
vitormiegsi <vhoa1995@hotmail.com>
2018-01-29 19:33:12 +0000

    asd

---
+++ controllers/veiculos.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(request, response){
+	//console.log(request.user);
+	//console.log(request.isAuthenticated());
+	response.set("Content-Type", "text/html");
+	response.render('./veiculos-item', {})
+});
+
+
+module.exports = router;
