435c8b3285098e8ba1d1d9a406bf269327d691af
naishes <32779815+naishes@users.noreply.github.com>
2018-01-29 21:19:46 +0000

    qwq

--- controllers/veiculos.route.js
+++ controllers/veiculos.route.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const model = require('../models/veiculos.model');
 
 router.get('/', function(request, response){
 	//console.log(request.user);
 	//console.log(request.isAuthenticated());
+	model.list(function (veiculos) {
 	response.set("Content-Type", "text/html");
-	response.render('./veiculos-item', {})
+	response.render('./veiculos-list', {
+		data : veiculos
+	})
+})
 });
+
 
 
 module.exports = router;
