7e7e1c6c021f6e624c737ef7b450462b126a56f9
vitormiegsi <vhoa1995@hotmail.com>
2018-01-29 20:01:13 +0000

    l

--- controllers/clientes.route.js
+++ controllers/clientes.route.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const model = require('../models/clientes.model');
+
 
 router.get('/', function(request, response){
 	//console.log(request.user);
 	//console.log(request.isAuthenticated());
+	model.list(function (clientes) {
+
 	response.set("Content-Type", "text/html");
-	response.render('./clientes-item', {})
+	response.render('./clientes-list', {
+
+		
+		clientes : clientes
+
+
+
+	})
+})
 });
+
 
 module.exports = router;
---
+++ models/clientes.model.js
@@ -0,0 +1,81 @@
+module.exports = {
+	list(callback) {
+		var sql = 'SELECT * from clientes';
+		global.connection.query(sql, function (error, rows, fields) {
+			if (error) throw error;
+			callback(rows);
+			
+		});
+	},
+
+	read(username, callback) {
+		var sql = "SELECT * from clientes where username=?";
+		global.connection.query(sql, [username], function (error, rows, fields) {
+			if (error) throw error;
+			callback(rows[0]);
+		});
+	},
+
+
+
+	create(data, callback) {
+		var sql = "INSERT INTO clientes (password_part, nome_participante, tel_participante, username_part, email_part) VALUES (?,?,?,?,?)";
+		global.connection.query(
+			sql, [data.password, data.name, data.numero, data.username, data.email],
+			function (error, rows, fields) {
+				if (error) throw error;
+				callback(rows[0]);
+			});
+	},
+
+
+
+
+
+
+
+
+	remove(username, callback) {
+		var sql = "DELETE from clientes WHERE username=?";
+		global.connection.query(sql, [username], function (error, rows, fields) {
+			if (error) throw error;
+			callback(rows);
+		});
+	},
+
+	/*
+	takenUsername(username, callback) {
+	    
+	    var sql = "SELECT password FROM participantes WHERE username=?"
+	    var user = global.connection.query(SQL, [username] );
+	                                       
+	                                       
+	                function bit (error, rows, fields){
+				     if (user === undefined)  {callback(true);
+	                    }else{
+	                        callback(false);
+	                    }
+	    }
+	},
+	                   
+	*/
+
+
+
+
+	//New
+	areValidCredentials(username, password, callback) {
+		var sql = "SELECT password FROM users WHERE username=?";
+		global.connection.query(sql, [username], function (error, rows, fields) {
+			if (error) throw error;
+			if (rows.length == 1 && rows[0].password === password) {
+				callback(true);
+			} else {
+				callback(false);
+			}
+		});
+	}
+
+	
+
+};
