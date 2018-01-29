7257be9dd68de56170449cd90c7f41dad6c3a74e
vitormiegsi <vhoa1995@hotmail.com>
2018-01-29 19:10:41 +0000

    yhn f

--- server.js
+++ server.js
@@ -105,6 +105,7 @@ app.use(function (request, response, next) {
 
 
 app.use('/public', express.static('public'));
+app.use('/index', require('./controllers/index.route'));
 
 //app.use('/users', require('./controllers/user.route'));
 
