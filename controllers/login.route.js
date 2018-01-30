6c87d83f9f0ddc4fb14f35d3c0c6d4862cb82694
vitormiegsi <vhoa1995@hotmail.com>
2018-01-30 21:54:43 +0000

    sdfg

--- controllers/login.route.js
+++ controllers/login.route.js
@@ -5,7 +5,7 @@ const usersModel = require('../models/user.model');
 router.get('/', function(request, response) {
 	//If is already authenticated don't show again the login form
 	if (request.isAuthenticated()) {
-		response.redirect('/');
+		response.redirect('/index');
 		return;
 	}
 	response.set("Content-Type", "text/html");
